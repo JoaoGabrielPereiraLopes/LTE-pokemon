c641e832a5d56f7360333bba274216a6025ecc4a
JoaoGabrielPereiraLopes <147850438+JoaoGabrielPereiraLopes@users.noreply.github.com>
2024-11-05 14:24:54 -0300

    Add files via upload

--- POKEMONS.py
+++ POKEMONS.py
@@ -5,7 +5,7 @@ import sqlite3
 
 caminho_arquivo = os.path.join('../database', 'Pokemon.csv')
 caminho_absoluto=os.path.abspath(caminho_arquivo)
-dados = pd.read_csv(caminho_absoluto)
+dados = pd.read_csv('/mnt/c/Users/labcaxias/Documents/DS202/BD/LTE-pokemon/dar')
 dados=pd.DataFrame(dados)
 filtro = dados.loc[dados['Name'].str.contains('Mega')]
 
